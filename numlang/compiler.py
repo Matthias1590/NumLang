5489c8ebae7d5c0670b07079b0947fed385cfb01
Matthias1590 <matthiasx95@gmail.com>
2021-11-05 11:54:40 +0100

    Added the stack class to simulate a stack (not implemented yet)

--- numlang/compiler.py
+++ numlang/compiler.py
@@ -5,18 +5,36 @@ from .lexer import Token, TokenTypes
 import random
 
 
+class Stack:
+    def __init__(self, size: int) -> None:
+        self.size = size
+        self.__stack = [random.randint(0, 2 ** 32 - 1) for _ in range(self.size)]
+
+    def push(
+        self,
+    ) -> None:
+        ...
+
+
 class Compiler:
     def __init__(self, operations: List[Token]) -> None:
         self.operations = operations
 
     def simulate(self) -> int:
-        memory = bytearray(1024)  # Allocate 1024 bytes of memory
+        # Allocate 1024 bytes of memory
+        memory = bytearray(1024)
         # Create a stack containing garbage values
-        stack = [random.randint(0, 2 ** 32 - 1)] * 1024
+        stack = Stack(1024)
+
+        # Simulate the operations
         for operation in self.operations:
             operation, arguments = operation.type, operation.type.arguments
             if isinstance(operation, TokenTypes.Operations.Push):
-                print("Push!")
+                stack.push(arguments[0])
+            elif isinstance(operation, TokenTypes.Operations.Pop):
+                stack.pop()
             else:
-                print("Idk")
+                print(stack)
+
+        # Return with exit code 0
         return 0
